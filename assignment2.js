df1272cf2968229f20cb7b64b3010252fdd8cedb
ElijahParkos <Elijah.J.Parkos@gmail.com>
2026-03-26 07:12:54 -0500

    Final version of html, css, and js files

--- assignment2.js
+++ assignment2.js
@@ -1,6 +1,6 @@
 let loaded_puzzle = 0; //index of the current puzzle we have loaded
 let current_colors = new Set(); //set of rgba color strings in the loaded image
-let selected_color = 'rgba(0,0,0,1)';
+let selected_color = '';
 
 //find whether the mouse is currently held down
 //this will be used to drag over multiple cells, rather than having to click one-by-one
@@ -20,8 +20,6 @@ const puzzles = [
     {name: "Island", id: "Island"}
 ];
 
-const STORAGE_KEY = "colorByNumbers_puzzles";
-
 window.addEventListener('storage',(e)=>{
     const changed_puzzle = e.key.split('_')[1];
     if(changed_puzzle==puzzles[loaded_puzzle].id) {
@@ -40,10 +38,13 @@ function setTitle(name) {
 //go through the pixel data of the current image and create a grid cell for each one
 function createGrid() {
     progress = 0;
+    setTitle(`${puzzles[loaded_puzzle].name} - 0%`);
     const grid = document.querySelector("#puzzle-grid");
     grid.innerHTML = "";
 
+    console.log(loaded_puzzle);
     let data = getImageData(puzzles[loaded_puzzle].id);
+    console.log(data);
     //using a set for colors, we can attempt to add every color in the data and it will automatically remove duplicates
     current_colors.clear();
     let current_storage = loadStorage();
@@ -65,7 +66,7 @@ function createGrid() {
         cell.dataset.c = color;
         
         grid.appendChild(cell);
-
+        
         if(current_storage[convertCoordsToIndex(x,y)] == '0') { //cell has not been clicked in storage
             cell.addEventListener('pointerdown', ()=> {
                 buttonClick(cell);
@@ -75,7 +76,12 @@ function createGrid() {
                 if(held_down) {
                     buttonClick(cell);
                 }
-            });   
+            });  
+            cell.addEventListener('keydown', (e)=>{
+                if(e.key=="Enter"||e.key==" ") {
+                    buttonClick(cell);
+                }
+            });
         } else { //cell has been clicked in storage
             cell.style.backgroundColor = color;
             cell.style.color = color;
@@ -101,7 +107,8 @@ function createGrid() {
 //connected to the sidebar puzzle buttons
 function changePage(puzzle_number) {
     loaded_puzzle = puzzle_number;
-    setTitle(puzzles[loaded_puzzle].name);
+    localStorage.setItem('colorByNumbers_Puzzle',loaded_puzzle);
+    selected_color = '';
     createGrid();
 }
 
@@ -115,6 +122,8 @@ function createPuzzleList() {
         const p = puzzles[i];
         const btn = document.createElement("button");
         btn.textContent = p.name;
+        btn.id = `puzzle-list-${i}`;
+        btn.dataset.index = i;
         btn.addEventListener("click", () => {changePage(i);});
         container.appendChild(btn);
     }
@@ -142,11 +151,10 @@ function createColorList(current_storage) {
         btn.textContent = (i+1);
         btn.dataset.c = color_numbers[i];
         
-        if(i==0) {
-            selected_color = color_numbers[i]; //set default color to whatever the first one is when we load a puzzle
-        }
         btn.style.backgroundColor = btn.dataset.c;
         btn.style.color = getContrastColor(color_numbers[i]);
+        btn.id = `color-selector-${i}`;
+        btn.dataset.index = i;
         btn.addEventListener("click", () => {
             switchColor(btn);
         });
@@ -186,7 +194,7 @@ function buttonClick(button) {
 
 //switches the currently selected color to the color of the selector button that was just pressed
 function switchColor(button) {
-    const color_selectors = document.querySelectorAll("#color-selector button")
+   const color_selectors = document.querySelectorAll("#color-selector button")
     for(let c of color_selectors) {
         if(c.classList.contains("selected")) {
             c.classList.remove("selected")
@@ -247,6 +255,11 @@ function modifyString(str,index,new_char) {
 //increments the total progress and checks when the puzzle is complete
 function updateProgress() {
     progress++;
+    let percent = Math.round((progress/256)*100);
+    if(progress<256 && percent==100) {
+        percent = 99;
+    }
+    setTitle(`${puzzles[loaded_puzzle].name} - ${percent}%`);
     if(progress>= 256) {
         const cells = document.querySelectorAll('.grid-cell');
         for(let i=0;i<cells.length;i++) {
@@ -256,7 +269,178 @@ function updateProgress() {
     }
 }
 
-changePage(loaded_puzzle);
-createPuzzleList();
 
-//document.documentElement.style.setProperty('--cell-border-color','transparent');
+//add click events for the settings menu
+const dialog = document.querySelector('dialog');
+const dialogBtn = document.querySelector('#close');
+dialogBtn.addEventListener('click',()=>{
+    dialog.close();
+});
+const settings = document.querySelector('#settings-button');
+document.querySelector('#settings-image').draggable = false;
+document.querySelector('#close-image').draggable = false;
+settings.addEventListener('click',()=>{
+    dialog.showModal();
+});
+const reset = document.querySelector('#reset-button');
+reset.addEventListener('click',()=>{
+    createStorage();
+    createGrid();
+})
+const default_palette = document.querySelector('#default-palette');
+const light_palette = document.querySelector('#light-palette');
+const dark_palette = document.querySelector('#dark-palette');
+default_palette.addEventListener('click',()=>{
+    changePalette(0);
+});
+light_palette.addEventListener('click',()=> {
+    changePalette(1);
+});
+dark_palette.addEventListener('click',()=>{
+    changePalette(2);
+});
+
+
+//changes the palette based on the number provided
+//connected to the palette buttons in the settings menu
+function changePalette(index) {
+    localStorage.setItem('colorByNumbers_Palette',index);
+    const s = document.documentElement.style;
+    if(index==0) {
+        s.setProperty('--font-color','#ffffff');
+        s.setProperty('--main-bg-color','#363d66');
+        s.setProperty('--second-bg-color','#040c40');
+        s.setProperty('--border-color','#1d2453');
+    } else if(index==1) {
+        s.setProperty('--font-color','#000000');
+        s.setProperty('--main-bg-color','#ffffff');
+        s.setProperty('--second-bg-color','#c8c8c8');
+        s.setProperty('--border-color','#969696');
+    } else {
+        s.setProperty('--font-color','#ffffff');
+        s.setProperty('--main-bg-color','#2b2b2b');
+        s.setProperty('--second-bg-color','#181C14');
+        s.setProperty('--border-color','#000000');
+    }
+}
+
+
+
+//load color palette from local storage
+//the Number() method will make it default to 0 if getItem returns null
+changePalette(Number(localStorage.getItem('colorByNumbers_Palette')));
+
+//run the functions which load the elements on the page
+//uses window.onload to wait for all the image elements to fully load before we attempt to read from them
+window.onload = ()=>{
+    changePage(Number(localStorage.getItem('colorByNumbers_Puzzle')));
+    createPuzzleList();
+};
+
+//gets key input, allowing the user to control focus using arrow keys 
+window.addEventListener('keydown',(e)=>{
+    let focus = document.activeElement;
+    if(e.key=="ArrowRight") {
+        if(focus.classList.value=='grid-cell') {
+            if(Number(focus.dataset.x)<15){
+                document.querySelector(`#cell-${Number(focus.dataset.x)+1}-${Number(focus.dataset.y)}`).focus();
+            } else {
+                document.querySelector('#settings-button').focus();
+            }
+        } else if(focus.id=='settings-button') {
+            document.querySelector('#puzzle-list-0').focus();
+        } else if(focus.id.includes('puzzle-list')) {
+            document.querySelector('#cell-0-0').focus();
+        } else if(focus.id.includes('color-selector')) {
+            const index = Number(focus.dataset.index);
+            if(index==2 || index==5 || index==8) {
+                document.querySelector('#cell-0-0').focus();
+            } else {
+                const next_selector = document.querySelector(`#color-selector-${index+1}`);
+                if(next_selector==null) {
+                    document.querySelector('#cell-0-0').focus();
+                } else {
+                    next_selector.focus();
+                }
+            }
+        }
+    } else if(e.key=="ArrowLeft") {
+        if(focus.classList.value=='grid-cell') {
+            if(Number(focus.dataset.x)>0){
+                document.querySelector(`#cell-${Number(focus.dataset.x)-1}-${Number(focus.dataset.y)}`).focus();
+            } else {
+                document.querySelector('#puzzle-list-0').focus();
+            }
+        } else if(focus.id=='settings-button') {
+            document.querySelector('#cell-15-0').focus();
+        } else if(focus.id.includes('puzzle-list')) {
+            document.querySelector('#settings-button').focus();
+        } else if(focus.id.includes('color-selector')) {
+            const index = Number(focus.dataset.index);
+            if(index==0 || index==3 || index==6) {
+                document.querySelector('#settings-button').focus();
+            } else {
+                document.querySelector(`#color-selector-${index-1}`).focus();
+            }
+        }
+    } else if(e.key=="ArrowDown") {
+        if(focus.classList.value=='grid-cell') {
+            if(Number(focus.dataset.y)<15){
+                document.querySelector(`#cell-${Number(focus.dataset.x)}-${Number(focus.dataset.y)+1}`).focus();
+            } else {
+                document.querySelector(`#cell-${Number(focus.dataset.x)}-0`).focus();
+            }
+        } else if(focus.id=='settings-button') {
+            document.querySelector('#cell-15-0').focus();
+        } else if(focus.id.includes('puzzle-list')) {
+            const next_puzzle = document.querySelector(`#puzzle-list-${Number(focus.dataset.index)+1}`);
+            if(next_puzzle==null) {
+                document.querySelector('#color-selector-0').focus();
+            } else {
+                next_puzzle.focus();
+            }
+        } else if(focus.id.includes('color-selector')) {
+            const index = Number(focus.dataset.index);
+            if(index==6 || index==7 || index==8) {
+                document.querySelector('#puzzle-list-0').focus();
+            } else {
+                const next_selector = document.querySelector(`#color-selector-${index+3}`);
+                if(next_selector==null) {
+                    document.querySelector('#puzzle-list-0').focus();
+                } else {
+                    next_selector.focus();
+                }
+            }
+        }
+    } else if(e.key=="ArrowUp") {
+        if(focus.classList.value=='grid-cell') {
+            if(Number(focus.dataset.y)>0){
+                document.querySelector(`#cell-${Number(focus.dataset.x)}-${Number(focus.dataset.y)-1}`).focus();
+            } else {
+                document.querySelector(`#cell-${Number(focus.dataset.x)}-15`).focus();
+            }
+        } else if(focus.id=='settings-button') {
+            document.querySelector('#cell-15-15').focus();
+        } else if(focus.id.includes('puzzle-list')) {
+            const next_puzzle = document.querySelector(`#puzzle-list-${Number(focus.dataset.index)-1}`);
+            if(next_puzzle==null) {
+                document.querySelector('#color-selector-6').focus();
+            } else {
+                next_puzzle.focus();
+            }
+        } else if(focus.id.includes('color-selector')) {
+            const index = Number(focus.dataset.index);
+            if(index==0 || index==1 || index==2) {
+                document.querySelector('#puzzle-list-5').focus();
+            } else {
+                const next_selector = document.querySelector(`#color-selector-${index-3}`);
+                if(next_selector==null) {
+                    document.querySelector('#puzzle-list-5').focus();
+                } else {
+                    next_selector.focus();
+                }
+            }
+        }
+    }
+
+});
